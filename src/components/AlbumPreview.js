e41710306c9bd2fdaf26d3d3da7c3e9f791cf404
hdaniel1 <hallada.daniel@gmail.com>
2019-07-09 23:19:45 -0400

    misc changes

--- src/components/AlbumPreview.js
+++ src/components/AlbumPreview.js
@@ -32,7 +32,7 @@ export default class AlbumPreview extends React.Component {
                     <Card.Content textAlign="center">
                         <Card.Header >{albumInfo.name}</Card.Header>
                         <Card.Meta >{albumInfo.artists[0].name}</Card.Meta>
-                        <Card.Description>Release Date: {`${monthNames[releaseDate.getMonth()]} ${releaseDate.getDate()}, ${releaseDate.getFullYear()}`}}</Card.Description>
+                        <Card.Description>Release Date: {`${monthNames[releaseDate.getMonth()]} ${releaseDate.getDate()}, ${releaseDate.getFullYear()}`}</Card.Description>
                             {/*If already listened to buttons*/}<br/>
                             {_.some(userAlbums, {"spotify_id": albumInfo.id, "listened_to": true}) ? 
                                 <Button color="orange" disabled >Listened!</Button>
--- src/components/PlaylistCard.js
+++ src/components/PlaylistCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Card, Image} from 'semantic-ui-react'
+import '../styles/Playlists.css';
 
 export default class PlaylistCard extends React.Component {
     render() {
@@ -10,7 +11,7 @@ export default class PlaylistCard extends React.Component {
                     <Image floated='left' size='small' src={playlist.playlist_image ? playlist.playlist_image : 'https://react.semantic-ui.com/images/wireframe/short-paragraph.png'} />
                     <Card.Header>{playlist.name}</Card.Header>
                     <Card.Meta>Playlist</Card.Meta>
-                    <Card.Header><b><u>Description:</u></b> {playlist.description}</Card.Header>
+                    <Card.Header id="playlist-description-header"><b><u>Description:</u></b><br/> {playlist.description}</Card.Header>
                     {playlistAlbums.length > 0 ? playlistAlbums.map(album => album.images ? <Image size="mini" src={album.images[2].url} alt="playlist-album-preview"/> : null) :null }
                 </Card.Content>
             </Card>
